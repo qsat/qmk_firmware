c8e9e4d37b58a4ee02f7a763fe77039249aa1292
SATOSHI ARAI <satoshiarai@SATOSHIs-MacBook-Pro.local>
2018-07-27 01:40:01 +0900

    update

--- keyboards/lets_split/keymaps/qsat/keymap.c
+++ keyboards/lets_split/keymaps/qsat/keymap.c
@@ -29,20 +29,20 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |  `   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Adjust| Ctrl | Alt  | GUI  |Lower | Enter|Space |Raise |  [   |   ]  |   -  |  =   |
+ * | ESC  | Ctrl | Alt  | GUI  |Lower | Enter|Space |Raise |  [   |   ]  |   -  |  =   |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_ESC,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+   KC_GRV,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
    CTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT), \
-   ADJUST,        KC_LCTL, KC_LGUI, KC_LALT, LOWER,   SFT_T(KC_ENT),  SFT_T(KC_SPC),  RAISE,   KC_LBRC, KC_LBRC, KC_MINS, KC_EQL\
+   KC_ESC,        KC_LCTL, KC_LGUI, KC_LALT, LOWER,   SFT_T(KC_ENT),  SFT_T(KC_SPC),  RAISE,   KC_LBRC, KC_LBRC, KC_MINS, KC_EQL\
 ),
 
 /* Colemak
