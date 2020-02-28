3ed2fcb1c8355aa02f740387d5cb419ae0956133
s_arai <s_arai@r.recruit.co.jp>
2020-02-28 19:09:28 +0900

    update

--- keyboards/lets_split/keymaps/qsat/keymap.c
+++ keyboards/lets_split/keymaps/qsat/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ortho_4x12( \
   _______, KC_EXLM,   KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN,    _______, \
   _______, S(KC_GRV), _______, _______, _______, _______, _______, KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR,    _______, \
-  _______, _______,   _______, _______, _______, _______, _______, S(KC_NUHS), S(KC_QUOT), KC_PIPE, S(KC_SLSH), _______, \
+  _______, KC_BSLS,   _______, _______, _______, _______, _______, S(KC_QUOT), S(KC_COMM), S(KC_DOT), S(KC_SLSH), _______, \
   KC_LEFT, KC_DOWN,   KC_UP,   KC_ESC,  _______, _______, KC_TAB,  _______,    KC_ESC,     KC_DOWN, KC_UP,      KC_RIGHT \
 ),
 
@@ -114,10 +114,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  _______, KC_GRV,  _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_QUOT, KC_BSLS, KC_SLSH, _______, \
-  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT \
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,      KC_0,    KC_DEL, \
+  _______, KC_GRV,  _______, _______, _______, _______, _______, KC_MINS, KC_EQL,     KC_LBRC,   KC_RBRC, _______, \
+  _______, KC_PIPE, _______, _______, _______, _______, _______, KC_QUOT, S(KC_COMM), S(KC_DOT), KC_SLSH, _______, \
+  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, _______, _______, KC_LEFT,    KC_DOWN,   KC_UP,   KC_RIGHT \
 ),
 
 /* Adjust (Lower + Raise)
--- keyboards/minidox/keymaps/default/keymap.c
+++ keyboards/minidox/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,       KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  CTL_T(KC_Z),KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_ENT, \
+  CTL_T(KC_Z),KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_ENT), \
                        KC_LGUI, LOWER,   KC_LSFT,      KC_SPC,  RAISE,   ALT_T(KC_BSPC)             \
 ),
 
@@ -62,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                `------'    `------'
  */
 [_RAISE] = LAYOUT( \
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  KC_GRV,  _______, _______, _______, _______,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
-  _______, _______, _______, _______, _______,      _______, KC_PIPE, KC_QUOT, KC_BSLS, KC_SLSH, \
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,       KC_9,      KC_0,    \
+  KC_GRV,  _______, _______, _______, _______,      _______, KC_MINS, KC_EQL,     KC_LBRC,   KC_RBRC, \
+  KC_PIPE, _______, _______, _______, _______,      _______, KC_QUOT, S(KC_COMM), S(KC_DOT), KC_SLSH, \
                     _______, _______, _______,      _______, _______, _______                    \
 ),
 
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                `------'    `------'
  */
 [_LOWER] = LAYOUT( \
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
-  KC_TILD,  _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
-  _______,  _______, _______, _______, _______,      _______, KC_BSLS, KC_DQT,  KC_PIPE, KC_QUES, \
+  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR,     KC_ASTR,     KC_LPRN,   KC_RPRN, \
+  KC_TILD,  _______, _______, _______, _______,      _______, KC_UNDS,    KC_PLUS,     KC_LCBR,   KC_RCBR, \
+  KC_BSLS,  _______, _______, _______, _______,      _______, S(KC_QUOT), S(KC_COMM),  S(KC_DOT), KC_QUES, \
                     _______, _______, _______,      KC_TAB,  _______, KC_ESC\
 ),
 
