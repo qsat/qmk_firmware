516ddb9afec35f6df02f3eb62bda8cf37be418bf
s_arai <s_arai@r.recruit.co.jp>
2020-03-13 16:11:31 +0900

    update miniaxe

--- keyboards/miniaxe/keymaps/default/keymap.c
+++ keyboards/miniaxe/keymaps/default/keymap.c
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    KC_LGUI, LOWER, MT(MOD_LCTL, KC_ESC),   MT(MOD_LSFT, KC_SPC), RAISE, MT(MOD_LALT, KC_BSPC)  \
+   KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,  KC_I,           KC_O,    KC_P, \
+   KC_A,        KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,  KC_K,           KC_L,    ALT_T(KC_SCLN), \
+   CTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,  KC_COMM,        KC_DOT,  CTL_T(KC_ENT), \
+                         KC_LGUI, LOWER,   KC_LSFT, KC_SPC, RAISE, ALT_T(KC_BSPC) \
 ),
 
 /* Raise
@@ -62,10 +62,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_RAISE] = LAYOUT( \
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
-  KC_TAB,  KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD,      KC_COLN, KC_DQUO, KC_GT,   KC_LCBR, KC_RCBR, \
-  KC_CAPS, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,       KC_SCLN, KC_QUOT, KC_LT,   KC_LBRC, KC_RBRC, \
-                    _______, _______, _______,      _______,  _______, _______\
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,      KC_0,\
+  KC_GRV,  _______, _______, _______, _______, _______, KC_MINS, KC_EQL,     KC_LBRC,   KC_RBRC, \
+  KC_PIPE, _______, _______, _______, _______, _______, KC_QUOT, S(KC_COMM), S(KC_DOT), KC_SLSH, \
+                    _______, _______, _______, _______,  _______, _______\
 ),
 
 /* Lower
@@ -81,10 +81,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  KC_TAB,  _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT, \
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, KC_DEL,      KC_BSPC, KC_PGUP, KC_PGDN, KC_BSLS, KC_QUOT, \
-                    _______, _______, _______,      _______, _______, _______                    \
+  KC_EXLM,   KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN,   KC_RPRN, \
+  S(KC_GRV), _______, _______, _______, _______, _______, KC_UNDS,    KC_PLUS,    KC_LCBR,   KC_RCBR, \
+  KC_BSLS,   _______, _______, _______, _______, _______, S(KC_QUOT), S(KC_COMM), S(KC_DOT), S(KC_SLSH), \
+                    _______, _______, _______,   _______, _______, _______                    \
 ),
 
 
@@ -102,8 +102,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, \
-  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, _______, _______, \
-  RESET,   _______, _______, _______, _______,      KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, \
+  KC_F11,  KC_F12,  _______, _______, _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
+  RESET,   _______, _______, _______, _______,      _______, _______, _______, _______, _______, \
                     _______, _______, _______,      _______,  _______, _______                   \
 )
 };
