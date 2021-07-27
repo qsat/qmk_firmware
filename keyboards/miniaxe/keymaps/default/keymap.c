0da5c0c54225f7012c4bb9b16c50bc8ebcd389c0
s_arai <s_arai@r.recruit.co.jp>
2021-07-28 01:29:09 +0900

    update

--- keyboards/miniaxe/keymaps/default/keymap.c
+++ keyboards/miniaxe/keymaps/default/keymap.c
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_QWERTY] = LAYOUT( \
-   KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,   KC_U,  KC_I,           KC_O,    KC_P, \
-   KC_A,        KC_S,    KC_D,    KC_F,    KC_G,          KC_H,   KC_J,  KC_K,           KC_L,    ALT_T(KC_SCLN), \
-   CTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,          KC_N,   KC_M,  KC_COMM,        KC_DOT,  CTL_T(KC_ENT), \
-                         KC_LGUI, LOWER,   SFT_T(KC_ENT), KC_SPC, RAISE, ALT_T(KC_BSPC) \
+   KC_Q,        KC_W,           KC_E,          KC_R,    KC_T,          KC_Y,   KC_U,  KC_I,         KC_O,           KC_P, \
+   KC_A,        KC_S,           KC_D,          KC_F,    KC_G,          KC_H,   KC_J,  KC_K,         KC_L,           ALT_T(KC_SCLN), \
+   CTL_T(KC_Z), ALT_T(KC_X),    KC_C,          KC_V,    KC_B,          KC_N,   KC_M,  KC_COMM,      ALT_T(KC_DOT),  CTL_T(KC_ENT), \
+                                GUI_T(KC_ESC), LOWER,   SFT_T(KC_ENT), KC_SPC, RAISE, GUI_T(KC_BSPC) \
 ),
 
 /* Raise
